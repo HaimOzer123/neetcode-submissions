44ef3b07a7620c34da74476eae715d8334ed2f66
Haim Ozer <121746738+HaimOzer123@users.noreply.github.com>
2026-05-13 11:45:12 +0300

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-0

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDifference(string s) {
+        std::vector<int> count(26,0);
+        for(char c:s){
+            count[c-'a']++;
+        }
+
+        int res = INT_MIN;
+        for (int odd : count){
+            if(odd ==0 || odd%2==0)continue;
+            for(int even:count){
+                if(even ==0||even%2==1)continue;
+                res = max(res,odd-even);
+            }
+        }
+        return res;
+        
+    }
+};
